290251a2f2849768514095e93e3a820f2f0c5134
dat4git <ghoshs@broadinstitute.org>
2016-12-13 12:41:46 -0500

    Update run_freebayes.sh

--- run_freebayes.sh
+++ run_freebayes.sh
@@ -1,5 +1,5 @@
-APPS_PATH=/ifs/labs/cccb/projects/cccb/apps
-# REFERENCE='/ifs/labs/cccb/projects/db/gatk/hg19/ucsc.hg19.fasta' 
+APPS_PATH=/bsnp/apps
+# REFERENCE='/bsnp/db/gatk/hg19/ucsc.hg19.fasta' 
 
 BAM_FILE=$1
 REGION=$2
@@ -14,9 +14,9 @@ GENOME=$4
 HG19='hg19'
 MM10='mm10'
 if   [ $GENOME == $HG19 ] 
-	then REFERENCE='/ifs/labs/cccb/projects/db/gatk/hg19/ucsc.hg19.fasta'
+	then REFERENCE='/bsnp/db/gatk/hg19/ucsc.hg19.fasta'
 elif [ $GENOME == $MM10 ] 
-	then REFERENCE='/ifs/labs/cccb/projects/db/genomes/mouse/build38/mm10.fa'
+	then REFERENCE='/bsnp/db/genomes/mouse/build38/mm10.fa'
 else echo "No genome selected!"; exit 1;
 fi
 
